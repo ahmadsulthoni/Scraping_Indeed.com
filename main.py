d957faf3990575cadbe5fceb84110198ed24aafa
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-02-27 09:16:03 +0700

    Beautifulsoup Import

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,6 +11,30 @@ params = {
 headers = { 'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 '}
 
 res = requests.get(url, params=params, headers=headers)
-soup = BeautifulSoup(res.text, 'html.parser')
 
-print(soup.prettify())
+
+#mendapatkan total page secara dinamis
+def get_total_pages():
+    params = {
+    'q' : 'Python Developer',
+    'l' : 'New York State',
+
+}
+    res = requests.get(url, params=params, headers=headers)
+
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
+
+    with open('temp/res.html','w+') as outfile:
+        outfile.write(res.text)
+        outfile.close()
+
+    #scraping step
+    soup = BeautifulSoup(res.text, 'html.parser')
+    print(soup.prettify())
+
+
+if __name__ == '__main__':
+    get_total_pages()
