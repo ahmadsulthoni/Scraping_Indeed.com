c2dd98edf6ae80762b1ffb1f7df993eeb48a3bfa
ahmad sulthoni <ahmadsulthoni.ext@gmail.com>
2022-02-27 08:54:30 +0700

    Beautifulsoup Import

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.indeed.com/jobs?'
+params = {
+    'q' : 'Python Developer',
+    'l' : 'New York State',
 
-res = requests.get(url)
-print(res.headers)
+}
+headers = { 'user-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 '}
+
+res = requests.get(url, params=params, headers=headers)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+print(soup.prettify())
